19750da080ac1f918b1c9bcf97f29d8af6aeb204
helio <heliosaian@gmail.com>
2015-12-16 23:55:49 +0200

    fap fap fap

---
+++ public/config.js
@@ -0,0 +1,56 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  paths: {
+    "npm:*": "vendor/npm/*",
+    "github:*": "vendor/github/*"
+  },
+
+  map: {
+    "ionic": "github:driftyco/ionic-bower@1.2.0",
+    "lodash": "npm:lodash@3.10.1",
+    "typescript": "npm:typescript@1.7.5",
+    "github:angular-ui/ui-router@0.2.13": {
+      "angular": "github:angular/bower-angular@1.4.8"
+    },
+    "github:angular/bower-angular-animate@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
+    },
+    "github:angular/bower-angular-sanitize@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
+    },
+    "github:driftyco/ionic-bower@1.2.0": {
+      "angular": "github:angular/bower-angular@1.4.8",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.8",
+      "angular-sanitize": "github:angular/bower-angular-sanitize@1.4.8",
+      "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
+      "css": "github:systemjs/plugin-css@0.1.20"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
+});
