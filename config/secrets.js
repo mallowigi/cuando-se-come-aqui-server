c858c23262f3d0ccd8088dc011db8e4ba2cc2f85
Elior <elior.b@dynamicyield.com>
2016-01-02 19:23:57 +0200

    fix keys

--- config/secrets.js
+++ config/secrets.js
@@ -27,9 +27,9 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
   mailgun: {
-    user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
-    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6',
-    key: process.env.MAILGUN_KEY || ''
+    user: process.env.MAILGUN_SMTP_LOGIN || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
+    password: process.env.MAILGUN_SMTP_PASSWORD || '29eldds1uri6',
+    key: process.env.MAILGUN_API_KEY || ''
   },
 
   mandrill: {
